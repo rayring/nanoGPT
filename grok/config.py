3271c36124ccb81a15d68f602602a47747bfccc5
xray <nimux.x@gmail.com>
2025-12-18 11:25:56 +0800

    set batch_size 128

--- grok/config.py
+++ grok/config.py
@@ -7,14 +7,14 @@ always_save_checkpoint = False  # 不用一直存模型，省空间
 
 # 2. 数据设置
 dataset = "grok"
-gradient_accumulation_steps = 1
-batch_size = 512  # 建议 512 或更大，直到显存占满 80% 左右
+gradient_accumulation_steps = 4
+batch_size = 128  # 建议 512 或更大，直到显存占满 80% 左右
 block_size = 32  # 建议 8 或 32 的倍数，以保证 GPU 处理效率
 
 # 3. 模型设置 (非常小的 Transformer)
 n_layer = 4
 n_head = 6
-n_embd = 384
+n_embd = n_head * 64
 dropout = 0.0
 bias = False  # 去掉 bias 更有利于数学任务
 
